25e7c985c1d81d769103dabbee152539bc3274c0
Aryank35 <aryan5935kishan@gmail.com>
2024-01-15 21:49:40 +0530

    added underline

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
             <ul className='flex flex-col md:flex-row justify-center items-center py-[30px] md:h-[100px] h-fit w-full space-y-4 md:space-y-0  md:space-x-5'>
               
               <Link to='/fest/CreditsPage'>
-              <li className='font-semibold text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Credits</li>
+              <li className='font-semibold underline text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Credits</li>
               </Link>
               <Link to='/coordinators'>
-              <li className='font-semibold text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Event Coordinators</li>
+              <li className='font-semibold underline text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Event Coordinators</li>
               </Link>
               <Link to='/help'>
-              <li className='font-semibold text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Help and Support Team</li>
+              <li className='font-semibold underline text-sm sm:text-2xl text-slate-300 hover:underline hover:text-slate-500 hover:cursor-pointer'>Help and Support Team</li>
               </Link>
             </ul>
         </div>
