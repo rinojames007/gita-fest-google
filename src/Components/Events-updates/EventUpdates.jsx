671841a1133a9d0c9dcdbdb8eb8ef17c3efa8935
Aryank35 <aryan5935kishan@gmail.com>
2024-02-01 16:28:08 +0530

    update Akanksha 2

--- src/Components/Events-updates/EventUpdates.jsx
+++ src/Components/Events-updates/EventUpdates.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import EventList from "./EventList";
-import upcomingEvents from "../../time.js";
 
 const EventUpdates = () => {
   // console.log(upcomingEvents);
@@ -15,19 +13,20 @@ const EventUpdates = () => {
           the fun.
         </p>
       </div>
-      <div className=" flex md:flex-row flex-col items-center py-7 px-3 md:py-[40px] md:px-[40px] md:space-x-6 mx-auto">
-        <div className="left hidden md:w-[50%] md:flex items-center mx-auto">
-          <div className="img-container w-full flex items-center rounded-xl shadow-xl shadow-black">
-            <img
-              src="https://i.postimg.cc/br6N8YFQ/Robo-wars.webp"
-              alt="Event-Picture"
-              className="rounded-xl flex items-center "
-            />
-          </div>
+      <div className=" flex md:flex-row flex-col items-center justify-center md:justify-between py-7 px-3 md:py-[40px] md:px-[40px] md:space-x-6 mx-auto">
+        <div className="left hidden md:w-[40%] md:flex items-center justify-center">
+        <div className="ilustrator flex justify-center w-full items-center py-[8px] ">
+                <img
+                  src="https://i.postimg.cc/8c8sR322/coordinator.webp"
+                  alt="ilustrator"
+                  className="w-[90%] md:w-[520px] rounded-3xl"
+                />
+              </div>
         </div>
         <div>
-          <div className="right md:w-[60%] w-full px-2 flex flex-col items-center space-y-5 py-3 md:py-0 mx-auto">
-            <div className="Event-lists w-full flex flex-col md:flex-row justify-around flex-wrap">
+          <div className="right w-full px-2 flex flex-col items-center space-y-5 py-3 md:py-0 mx-auto">
+            <div className="Event-lists w-full flex flex-col md:flex-row justify-center flex-wrap">
+            
               {/* {upcomingEvents.map((item) => (
               <EventList
                 key={item.key}
@@ -37,16 +36,39 @@ const EventUpdates = () => {
                 date={item.date}
               />
             ))} */}
-              <p className="BlinkText text-xl md:text-4xl mb-[20px]  font-bold text-white text-center pt-[15px] md:pt-[20px] UpcommingEvents">
+              {/* <p className="BlinkText text-xl md:text-4xl mb-[20px]  font-bold text-white text-center pt-[15px] md:pt-[20px] UpcommingEvents">
                 To be Announced...
+              </p> */}
+
+              
+            </div>
+            <div className=" w-full flex flex-col items-center justify-center space-y-3 text-[#721c24] text-center bg-[#f8d7da] p-[15px] rounded-lg border-[2px] shadow-xl shadow-black border-[#f4a1aa]">
+              <h2 className="text-2xl font-bold text-center w-full">
+                Audition for Odissi and Semi-Classical
+              </h2>
+              <p className="text-lg font-semibold">
+                <strong className="text-xl font-bold">Audition Date:</strong>{" "}
+                06/02/2024
               </p>
-              <div className="ilustrator flex justify-center w-full items-center py-[8px] ">
-                <img
-                  src="https://i.postimg.cc/8c8sR322/coordinator.webp"
-                  alt="ilustrator"
-                  className="w-[80%] md:w-[420px]"
-                />
-              </div>
+              <p>If anyone is interested, contact to:</p>
+              <ul>
+                <li>
+                  <strong>Coordinator:</strong> Kaibalya Ray (4th Year)
+                </li>
+
+                <li>
+                  <strong>Contact Number:</strong>{" "}
+                  <span className="font-semibold pl-3">7681011068</span>
+                </li>
+                <li className="mt-4">
+                  <strong>Faculty Coordinator:</strong> Dr. Sushmita Dash
+                </li>
+
+                <li>
+                  <strong>Contact Number:</strong>
+                  <span className="font-semibold pl-3">8895435809</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
