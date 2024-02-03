9587eb525d0bd050053ef1c0eab0431b74c9f296
Aryank35 <aryan5935kishan@gmail.com>
2024-02-03 20:35:30 +0530

    added Coordinators (Faculty Akanksha )

--- src/Components/Coordinator/CoordinatorList.jsx
+++ src/Components/Coordinator/CoordinatorList.jsx
@@ -9,8 +9,25 @@ const CoordinatorList = () => {
   };
 
   return (
-    <div className=" flec flex-col justify-center mx-auto items-center w-full h-full py-10 space-y-[40px] ">
+    <div className=" flec flex-col justify-center mx-auto items-center w-full h-full py-5 space-y-[40px] ">
+      <p className="UpcommingEvents text-2xl md:text-4xl text-center font-semibold text-white">
+          Faculty Coordinators (Akanksha 2024)
+        </p>
       {loading && (
+        <div className="flex flex-col justify-center items-center h-[220px]">
+          <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500 text-white font-semibold text-lg  mt-[80px]"></div>
+          <div className="loader text-center text-white font-semibold text-xl mt-6 ">
+            Loading...
+          </div>
+        </div>
+      )}
+      <iframe
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT7TIIaUjeAHJNkoEhiCbR8nd__y25IIyla_6oA9lJsFaZmulRZp3sYt3e1F64FNW74vPMSMbIZCYGP/pubhtml?widget=true&amp;headers=false"
+        className="md:w-[70%]  2xl:w-[50%] w-[95%] md:h-[300px] h-[500px] mx-auto md:rounded-xl rounded-md"
+        onLoad={handleIframeLoad}
+      ></iframe>
+
+      {/* {loading && (
         <div className="flex flex-col justify-center items-center h-[220px]">
           <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-500 text-white font-semibold text-lg  mt-[80px]"></div>
           <div className="loader text-center text-white font-semibold text-xl mt-6 ">
@@ -49,9 +66,7 @@ const CoordinatorList = () => {
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQhmOweX5f4fYUjHCIS5SrM61FbJ8LpNKgZB1ykPCrLcg1HCc2ESmM8nbmbnl7RnltWD3BkcS0S8d1V/pubhtml?widget=true&amp;headers=false"
         className="md:w-[70%]  2xl:w-[50%] w-[95%] md:h-[300px] h-[500px] mx-auto md:rounded-xl rounded-md"
         onLoad={handleIframeLoad}
-      ></iframe>
-
-     
+      ></iframe> */}
     </div>
   );
 };
